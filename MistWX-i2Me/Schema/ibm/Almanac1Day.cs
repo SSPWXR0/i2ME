bdfba02d2727cd35dd859eb9232b0857b01e8973
PicelBoi <picelboi@picelboi.xyz>
2025-12-28 22:17:30 -0500

    wait, it's a list all along? (Fix schema for Almanac1Day)

--- MistWX-i2Me/Schema/ibm/Almanac1Day.cs
+++ MistWX-i2Me/Schema/ibm/Almanac1Day.cs
@@ -2,18 +2,18 @@ namespace MistWX_i2Me.Schema.ibm;
 
 public class Almanac1DayResponse
 {
-    public string almanacInterval { get; set; }
-    public string almanacRecordDate { get; set; }
-    public int almanacRecordPeriod { get; set; }
-    public int almanacRecordYearMax { get; set; }
-    public int almanacRecordYearMin { get; set; }
-    public double precipitationAverage { get; set; }
-    public double snowAccumulationAverage { get; set; }
-    public string stationId { get; set; }
-    public string stationName { get; set; }
-    public int temperatureAverageMax { get; set; }
-    public int temperatureAverageMin { get; set; }
-    public int temperatureAverageMean { get; set; }
-    public int temperatureRecordMax { get; set; }
-    public int temperatureRecordMin { get; set; }
+    public List<string> almanacInterval { get; set; }
+    public List<string> almanacRecordDate { get; set; }
+    public List<int> almanacRecordPeriod { get; set; }
+    public List<int> almanacRecordYearMax { get; set; }
+    public List<int> almanacRecordYearMin { get; set; }
+    public List<double> precipitationAverage { get; set; }
+    public List<double> snowAccumulationAverage { get; set; }
+    public List<string> stationId { get; set; }
+    public List<string> stationName { get; set; }
+    public List<int> temperatureAverageMax { get; set; }
+    public List<int> temperatureAverageMin { get; set; }
+    public List<int> temperatureAverageMean { get; set; }
+    public List<int> temperatureRecordMax { get; set; }
+    public List<int> temperatureRecordMin { get; set; }
 }
