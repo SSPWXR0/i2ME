a9e151823272c81b750610a489fa47597c5a6dbb
PicelBoi <picelboi@picelboi.xyz>
2026-01-17 18:04:47 -0500

    add current observations

---
+++ MistWX-i2Me/API/Products/CurrentObservationsProduct2.cs
@@ -0,0 +1,24 @@
+using MistWX_i2Me.Schema.ibm;
+using MistWX_i2Me.Schema.System;
+
+namespace MistWX_i2Me.API.Products;
+
+public class CurrentObservationsProduct2 : Base
+{
+    public CurrentObservationsProduct2()
+    {
+        this.RecordName = "CurrentObservations2";
+        this.DataUrl =
+            "https://api.weather.com/v1/geocode/{lat}/{long}/observations.json?language={lang}&units={unit}&apiKey={apiKey}";
+    }
+
+    public async Task<List<GenericResponse<CurrentObservations2Response>>> Populate(string[] locations)
+    {
+        return await GetJsonData<CurrentObservations2Response>(locations);
+    }
+
+    public async Task<GenericResponse<CurrentObservations2Response>> Receive(LFRecordLocation locations)
+    {
+        return await GetJsonDataLFR<CurrentObservations2Response>(locations);
+    }
+}
---
+++ MistWX-i2Me/Schema/ibm/Observations2.cs
@@ -0,0 +1,10 @@
+
+public class CurrentObservations2Obs
+{
+    public string? key { get; set; }
+}
+
+public class CurrentObservations2Response
+{
+    public CurrentObservations2Obs? observation { get; set; }
+}
