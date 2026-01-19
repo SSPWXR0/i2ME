dc24e10d57aa43fe397604099a70452933d6d7f6
PicelBoi <picelboi@picelboi.xyz>
2026-01-19 14:48:25 -0500

    Fix warnings in WateringNeeds schema

--- MistWX-i2Me/Schema/ibm/WateringNeeds.cs
+++ MistWX-i2Me/Schema/ibm/WateringNeeds.cs
@@ -6,10 +6,10 @@ namespace MistWX_i2Me.Schema.ibm;
 public class WateringNeedsMetadata { 
 
 	[XmlElement(ElementName="language")] 
-	public string Language { get; set; } 
+	public string? Language { get; set; } 
 
 	[XmlElement(ElementName="transactionId")] 
-	public string TransactionId { get; set; } 
+	public string? TransactionId { get; set; } 
 
 	[XmlElement(ElementName="version")] 
 	public int Version { get; set; } 
@@ -32,47 +32,47 @@ public class WateringNeedsMetadata {
 public class wateringNeedsIndex { 
 
 	[XmlElement(ElementName="wateringNeedsIndex")] 
-	public List<int> wtrNeedsIdx { get; set; } 
+	public List<int>? wtrNeedsIdx { get; set; } 
 }
 
 [XmlRoot(ElementName="wateringNeedsCategory")]
 public class wateringNeedsCategory { 
 
 	[XmlElement(ElementName="wateringNeedsCategory")] 
-	public List<string> wtrNeedsCat { get; set; } 
+	public List<string>? wtrNeedsCat { get; set; } 
 }
 
 [XmlRoot(ElementName="wateringNeedsIndex12hour")]
 public class wateringNeedsIndex12hour { 
 
 	[XmlElement(ElementName="fcstValid")] 
-	public FcstValid FcstValid { get; set; } 
+	public FcstValid? FcstValid { get; set; } 
 
 	[XmlElement(ElementName="fcstValidLocal")] 
-	public FcstValidLocal FcstValidLocal { get; set; } 
+	public FcstValidLocal? FcstValidLocal { get; set; } 
 
 	[XmlElement(ElementName="dayInd")] 
-	public DayInd DayInd { get; set; } 
+	public DayInd? DayInd { get; set; } 
 
 	[XmlElement(ElementName="num")] 
-	public Num Num { get; set; } 
+	public Num? Num { get; set; } 
 
 	[XmlElement(ElementName="daypartName")] 
-	public DaypartName DaypartName { get; set; } 
+	public DaypartName? DaypartName { get; set; } 
 
 	[XmlElement(ElementName="wateringNeedsIndex")] 
-	public wateringNeedsIndex wateringNeedsIndex { get; set; } 
+	public wateringNeedsIndex? wateringNeedsIndex { get; set; } 
 
 	[XmlElement(ElementName="wateringNeedsCategory")] 
-	public wateringNeedsCategory wateringNeedsCategory { get; set; } 
+	public wateringNeedsCategory? wateringNeedsCategory { get; set; } 
 }
 
 [XmlRoot(ElementName="daypartForecastResponse")]
 public class WateringNeedsResponse { 
 
 	[XmlElement(ElementName="metadata")] 
-	public WateringNeedsMetadata Metadata { get; set; } 
+	public WateringNeedsMetadata? Metadata { get; set; } 
 
 	[XmlElement(ElementName="wateringNeedsIndex12hour")] 
-	public wateringNeedsIndex12hour wateringNeedsIndex12hour { get; set; } 
+	public wateringNeedsIndex12hour? wateringNeedsIndex12hour { get; set; } 
 }
