b298402da4c73322adcef41dbd501697513b3278
PicelBoi <picelboi@picelboi.xyz>
2026-01-19 14:30:45 -0500

    Fix warnings in AchesPain

--- MistWX-i2Me/Schema/ibm/AchesPain.cs
+++ MistWX-i2Me/Schema/ibm/AchesPain.cs
@@ -6,10 +6,10 @@ namespace MistWX_i2Me.Schema.ibm;
 public class AchesPainMetadata { 
 
 	[XmlElement(ElementName="language")] 
-	public string Language { get; set; } 
+	public string? Language { get; set; } 
 
 	[XmlElement(ElementName="transactionId")] 
-	public string TransactionId { get; set; } 
+	public string? TransactionId { get; set; } 
 
 	[XmlElement(ElementName="version")] 
 	public int Version { get; set; } 
@@ -32,47 +32,47 @@ public class AchesPainMetadata {
 public class AchesPainsIndex { 
 
 	[XmlElement(ElementName="achesPainsIndex")] 
-	public List<int> AchesPainsIdex { get; set; } 
+	public List<int>? AchesPainsIdex { get; set; } 
 }
 
 [XmlRoot(ElementName="achesPainsCategory")]
 public class AchesPainsCategory { 
 
 	[XmlElement(ElementName="achesPainsCategory")] 
-	public List<string> AchesPainsCat { get; set; } 
+	public List<string>? AchesPainsCat { get; set; } 
 }
 
 [XmlRoot(ElementName="achesPainsIndex12hour")]
 public class AchesPainsIndex12Hour { 
 
 	[XmlElement(ElementName="fcstValid")] 
-	public FcstValid FcstValid { get; set; } 
+	public FcstValid? FcstValid { get; set; } 
 
 	[XmlElement(ElementName="fcstValidLocal")] 
-	public FcstValidLocal FcstValidLocal { get; set; } 
+	public FcstValidLocal? FcstValidLocal { get; set; } 
 
 	[XmlElement(ElementName="dayInd")] 
-	public DayInd DayInd { get; set; } 
+	public DayInd? DayInd { get; set; } 
 
 	[XmlElement(ElementName="num")] 
-	public Num Num { get; set; } 
+	public Num? Num { get; set; } 
 
 	[XmlElement(ElementName="daypartName")] 
-	public DaypartName DaypartName { get; set; } 
+	public DaypartName? DaypartName { get; set; } 
 
 	[XmlElement(ElementName="achesPainsIndex")] 
-	public AchesPainsIndex AchesPainsIndex { get; set; } 
+	public AchesPainsIndex? AchesPainsIndex { get; set; } 
 
 	[XmlElement(ElementName="achesPainsCategory")] 
-	public AchesPainsCategory AchesPainsCategory { get; set; } 
+	public AchesPainsCategory? AchesPainsCategory { get; set; } 
 }
 
 [XmlRoot(ElementName="daypartForecastResponse")]
 public class AchesPainResponse { 
 
 	[XmlElement(ElementName="metadata")] 
-	public AchesPainMetadata Metadata { get; set; } 
+	public AchesPainMetadata? Metadata { get; set; } 
 
 	[XmlElement(ElementName="achesPainsIndex12hour")] 
-	public AchesPainsIndex12Hour AchesPainsIndex12hour { get; set; } 
+	public AchesPainsIndex12Hour? AchesPainsIndex12hour { get; set; } 
 }
