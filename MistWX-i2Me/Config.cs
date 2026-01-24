32019131c1c2b45f6a95e8f976204624e5f16df7
PicelBoi <picelboi@picelboi.xyz>
2026-01-23 19:17:24 -0500

    Disable AirportDelays for now

--- MistWX-i2Me/Config.cs
+++ MistWX-i2Me/Config.cs
@@ -504,7 +504,7 @@ public class Config
         [XmlElement] public bool DHRecord { get; set; } = false;
         [XmlElement] public bool HolidayMapping { get; set; } = true;
         [XmlElement] public bool TIRecord { get; set; } = true;
-        [XmlElement] public bool AirportDelays { get; set; } = true;
+        [XmlElement] public bool AirportDelays { get; set; } = false;
         [XmlElement] public bool DerivedHTRecord { get; set; } = true;
     }
 }
